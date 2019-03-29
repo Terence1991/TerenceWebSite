8e305fa31854c2e3803ef84490e6f932a414ce8e
terence1991 <cplterence@gmail.com>
2019-03-29 11:36:15 -0400

    background done

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ const AppWrapper = styled.div`
   background-image: url('http://images.performgroup.com/di/library/sporting_news/30/47/eli-manning-101016-getty-ftrjpg_16avucspkz0ot1q6mz30veq348.jpg?t=-1372926513&w=960&quality=70');
   background-repeat: no-repeat;
   background-position: center center;
+  background-size: cover; 
   font-family: 'Roboto', sans-serif;
   height: 100vh;
   width: 100vw;
