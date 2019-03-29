c1d6e146e29df40a2af0fdd47642b0344491c008
terence1991 <cplterence@gmail.com>
2019-03-29 08:32:02 -0400

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import Header from './Header.js'
 import styled from 'styled-components'
 
 const AppWrapper = styled.div`
+  background-image: url('https://images.pexels.com/photos/242236/pexels-photo-242236.jpeg?cs=srgb&dl=background-cement-concrete-242236.jpg&fm=jpg');
   font-family: 'Roboto', sans-serif;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 `
 const App = () => {
   return (
   <AppWrapper>
-    <div>
       <Header/>
-    </div>
   </AppWrapper> 
   )
 }
