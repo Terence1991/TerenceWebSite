a1151e0d633b8bdb1b67a79b1f49d9bfeccf9297
terence1991 <cplterence@gmail.com>
2019-03-29 13:33:45 -0400

    browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import Header from './Header.js'
-import Contact from '../screens/Contact.js'
-import styled from 'styled-components'
+import React from 'react';
+import Header from './Header.js';
+import Contact from '../screens/Contact.js';
+import Portfolio from '../screens/'
+
+import {BrowserRouter, Route} from 'react-router-dom';
+import styled from 'styled-components';
 
 const AppWrapper = styled.div`
   background-image: url('http://images.performgroup.com/di/library/sporting_news/30/47/eli-manning-101016-getty-ftrjpg_16avucspkz0ot1q6mz30veq348.jpg?t=-1372926513&w=960&quality=70');
@@ -14,7 +17,11 @@ const AppWrapper = styled.div`
 `
 const App = () => {
   return (
-  <AppWrapper>
+  <AppWrapper> 
+    <BrowserRouter>
+    <Route path="/contact" exact component={Contact}/>
+    <Route path="/portfolio" exact component={Portfolio}/>
+    </BrowserRouter> 
     <Header/>
     <Contact/> 
   </AppWrapper>
