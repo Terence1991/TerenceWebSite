826541f0e8ec581b74820a58edc7d2d12f8ae800
terence1991 <cplterence@gmail.com>
2019-03-30 18:52:42 -0400

    history added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,31 @@ import Contact from '../screens/Contact.js';
 import Portfolio from '../screens/Portfolio.js'
 import About from '../screens/About.js'
 import Services from '../screens/Services.js'
+import PropTypes from 'prop-types'
+import { createBrowserHistory } from 'history';
 
 class Header extends React.Component {
-  handleClickH() {
-    this.props.history.push('/')
-}
+  state = {url: ''}
+
+ handleClick = () => {
+    this.state.history.pushState(null, '/about');
+  }
 
 render() {
+  console.log(this.state)
   return (
     <BrowserRouter>
       <Route path="/contact" exact component={Contact}/>
       <Route path="/portfolio" exact component={Portfolio}/>
       <Route path="/about" exact component={About}/>
-      <Route path='services' exact component={Services}/>
-       <button onClick={this.handleClickH}>Home</button>
-      <button onClick={this.handleclickA} >About</button>
-      <button onClick={this.handleClickP}>Portfolio</button>
-      <button onClick={this.handleClickS}>Services</button>
-      <button onClick={this.handleClickC}>Contact</button>
+      <Route path='/services' exact component={Services}/>
+       {/* <button onClick={this.handleClick('/')}>Home</button> */}
+      <button onClick={this.handleClick}>About</button>
+      {/* <button onClick={this.handleClick('/portfolio')}>Portfolio</button>
+      <button onClick={this.handleClick('/services')}>Services</button>
+      <button onClick={this.handleClick('/contact')}>Contact</button> */}
     </BrowserRouter> 
   )
   }
 }
-export default withRouter(Header);
+export default createBrowserHistory(Header);
