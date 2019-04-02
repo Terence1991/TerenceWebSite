901f1c9dc119772ba756a8539cd882baecfe39e8
terence1991 <cplterence@gmail.com>
2019-04-02 12:35:07 -0400

    refactored header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,79 +6,30 @@ import About from '../screens/About.js'
 import Services from '../screens/Services.js'
 
 class Header extends React.Component {
-  state = {url: '/about', isPagedChanged: false}
-
-  componentDidMount() {
-   this.setState({isPagedChanged: true}) 
-  }
-
- handleClickAbout = () => {
+ 
+  handleClick = (url) => {
    let { history } = this.props
      history.push({
-       pathname: "/about", 
+       pathname: url, 
        search:  null
      })
      setTimeout(function(){
       window.location.reload(1);
    }, 1000);
 }
-  handleClickHome = () => {
-    let { history } = this.props
-    history.push({
-      pathname: '/',
-      search: null
-    })
-    setTimeout(function(){
-      window.location.reload(1);
-   }, 1000);
-  }
-
-  handleClickPortfolio = () => {
-    let { history } = this.props
-    history.push({
-      pathname: '/portfolio',
-      search: null
-    })
-    setTimeout(function(){
-      window.location.reload(1);
-   }, 1000);
-  }
-
-  handleClickServices = () => {
-    let { history } = this.props
-    history.push({
-      pathname: '/services',
-      search: null
-    })
-    setTimeout(function(){
-      window.location.reload(1);
-   }, 1000);
-  } 
-
-  handleClickContact = (url) => {
-    let { history } = this.props
-    history.push({
-      pathname: '/contact',
-      search: null
-    })
-    setTimeout(function(){
-      window.location.reload(1);
-   }, 1000);
-  } 
 
 render() {
-  console.log(this.state)
   return (
     <BrowserRouter>
       <Route path="/contact" exact component={Contact}/>
       <Route path="/portfolio" exact component={Portfolio}/>
       <Route path="/about" exact component={About}/>
       <Route path='/services' exact component={Services}/>
-      <button onClick={this.handleClickHome.bind(this)}>Home</button>
-      <button onClick={this.handleClickAbout.bind(this.state.url)}>About</button>
-      <button onClick={this.handleClickPortfolio.bind(this)}>Portfolio</button>
-      <button onClick={this.handleClickServices.bind(this)}>Services</button>
-      <button onClick={this.handleClickContact.bind(this)}>Contact</button> 
+      <button onClick={() => this.handleClick('/home')}>Home</button> 
+      <button onClick={() => this.handleClick('/about')}>About</button>
+      <button onClick={() => this.handleClick('/portfolio')}>Portfolio</button>
+      <button onClick={() => this.handleClick('/services')}>Services</button>
+      <button onClick={() => this.handleClick('/contact')}>Contact</button> 
     </BrowserRouter> 
   )
   }
